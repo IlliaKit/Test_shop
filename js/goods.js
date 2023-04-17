65f4d43763625dc628ab14768417c42ff4dc5ea2
Illia Yanchenko <79405414+IlliaKit@users.noreply.github.com>
2023-04-17 13:00:46 +0200

    add

---
+++ js/goods.js
@@ -0,0 +1,5 @@
+const GOODS = [
+  { id: 1, name: "Уроки по HTML", price: 1000, imgSrc: "img/html.png" },
+  { id: 2, name: "Уроки по CSS", price: 1500, imgSrc: "img/css.png" },
+  { id: 3, name: "Уроки по JS", price: 5000, imgSrc: "img/js.png" },
+];
